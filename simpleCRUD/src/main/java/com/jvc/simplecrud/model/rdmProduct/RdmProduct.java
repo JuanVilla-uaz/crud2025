751b9382ce0f1efe091c86dd762c369bc4c7a232
jlvilla@uaz.edu.mx <jlvilla@uaz.edu.mx>
2025-01-08 19:05:44 -0600

    Add RdmProduct

---
+++ simpleCRUD/src/main/java/com/jvc/simplecrud/model/rdmProduct/RdmProduct.java
@@ -0,0 +1,19 @@
+package com.jvc.simplecrud.model.rdmProduct;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RdmProduct {
+    private long id;
+    private String title;
+    private String description;
+    private double price;
+    private int stock;
+    private String category;
+
+}
