2c83fe3f8801aaba0a5d2d3427d33b25c1351eef
jlvilla@uaz.edu.mx <jlvilla@uaz.edu.mx>
2025-01-08 19:05:06 -0600

    Add ProductMappers

---
+++ simpleCRUD/src/main/java/com/jvc/simplecrud/mappers/ProductMappers.java
@@ -0,0 +1,17 @@
+package com.jvc.simplecrud.mappers;
+
+import com.jvc.simplecrud.model.product.Product;
+import com.jvc.simplecrud.model.rdmProduct.RdmProduct;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ProductMappers {
+
+    ProductMappers INSTANCE = Mappers.getMapper(ProductMappers.class);
+
+    @Mapping(source = "title", target = "name")
+    Product rdmProductToProduct(RdmProduct product);
+
+}
